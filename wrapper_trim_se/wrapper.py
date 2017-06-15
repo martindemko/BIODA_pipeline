858b2eb7d2497a3677dbc01db86cb2ad14cc4247
martindemko <325073@mail.muni.cz>
2017-06-15 16:50:40 +0200

    removes old wrapper for trim_se

--- wrapper_trim_se/wrapper.py
+++
@@ -1,6 +0,0 @@
-
-from snakemake.shell import shell
-
-TRIMMOMATIC = "/mnt/nfs/shared/999993-Bioda/scripts/martin/test/Tools/trimmomatic-master/classes/trimmomatic.jar"
-
-shell(""" java -jar {TRIMMOMATIC} SE -threads {snakemake.threads} {snakemake.params.phred} {snakemake.input.reads} {snakemake.output.clean} LEADING:{snakemake.params.leading} TRAILING:{snakemake.params.trailing} CROP:{snakemake.params.crop} SLIDINGWINDOW:{snakemake.params.slid_w_1}:{snakemake.params.slid_w_2} MINLEN:{snakemake.params.minlen} > {snakemake.log.run} 2>&1 """)
